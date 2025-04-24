f5c302550c1efd7fff3ac487562851f7f91351b4
artificialrose <129745681+artificialrose@users.noreply.github.com>
2025-04-24 14:42:42 +0700

    Update finalproject.sql

--- data warehouse/finalproject.sql
+++ data warehouse/finalproject.sql
@@ -1,15 +1,3 @@
-CREATE DATABASE "FinalProject"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.utf8'
-    LC_CTYPE = 'en_US.utf8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
-
-USE DATABASE FinalProject;
-
 CREATE TABLE	DimDate	(
 dateid	INT	,
 date	DATE	,
@@ -20,19 +8,72 @@ Month	INT	,
 Monthname	VARCHAR(20)	,
 Day	INT	,
 Weekday	INT	,
-WeekdayName	VARCHAR(20)	)
+WeekdayName	VARCHAR(20)	);
 
-CREATE TABLE	DimTruck	(
+
+
+CREATE TABLE DimTruck	(
+Truckid	INT	,
+TruckType	VARCHAR(255)	);
+
+
+CREATE TABLE DimStation		(
+Stationid	INT	,
+City	VARCHAR(255)	);
+
+
+CREATE TABLE	FactTrips	(
 Tripid	INT	,
 Dateid	INT	,
 Stationid	INT	,
 Truckid	INT	,
-Wastecollected	FLOAT(2)	)
+Wastecollected	FLOAT(2)	);
 
-CREATE TABLE	DimStation	(
-Truckid	INT	,
-TruckType	VARCHAR(255)	)
 
-CREATE TABLE	FactTrips	(
-Stationid	INT	,
-City	VARCHAR(255)	)
+SELECT * FROM DimDate LIMIT 5;
+
+SELECT * FROM DimTruck LIMIT 5;
+
+SELECT * FROM DimStation LIMIT 5;
+
+SELECT * FROM FactTrips LIMIT 5;
+
+
+SELECT f.stationid, tr.trucktype, SUM(f.wastecollected) AS total_Wastecollected
+FROM facttrips f
+LEFT JOIN dimtruck tr ON f.truckid = tr.truckid
+GROUP BY f.stationid, tr.trucktype;
+
+
+SELECT f.stationid, tr.trucktype, SUM(f.wastecollected) AS total_Wastecollected
+FROM facttrips f
+LEFT JOIN dimtruck tr ON f.truckid = tr.truckid
+GROUP BY GROUPING SETS (f.stationid, tr.trucktype);
+
+
+SELECT d.year, s.city, f.stationid, SUM(f.wastecollected) AS total_wastecollected
+FROM facttrips f
+LEFT JOIN dimdate d ON f.dateid = d.dateid
+JOIN dimstation s ON f.stationid = s.stationid
+GROUP BY ROLLUP (d.year, s.city, f.stationid)
+ORDER BY d.year, s.city, f.stationid;
+
+SELECT d.year, s.city, f.stationid, AVG( f.wastecollected ) AS average_wastecollected
+FROM facttrips f 
+LEFT JOIN dimdate d ON f.dateid = d.dateid
+join dimstation s ON f.stationid = s.stationid 
+GROUP BY CUBE (d.year, s.city, f.stationid)
+ORDER BY (d.year, s.city, f.stationid);
+
+CREATE MATERIALIZED VIEW  max_waste_stats (city, stationid, trucktype, max_wastecollected ) AS 
+SELECT s.city, f.stationid, tr.trucktype, MAX(f.wastecollected) AS max_wastecollected 
+FROM facttrips f
+LEFT JOIN dimtruck tr ON f.truckid = tr.truckid
+LEFT JOIN dimstation s ON f.stationid = s.stationid 
+GROUP BY (s.city, f.stationid, tr.trucktype);
+
+SELECT * FROM max_waste_stats;
+
+
+
+
