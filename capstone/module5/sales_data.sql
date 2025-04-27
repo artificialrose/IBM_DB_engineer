3a6d1d7a1f2a05a11f0cc8272080c9bc69afff53
artificialrose <129745681+artificialrose@users.noreply.github.com>
2025-04-28 00:13:28 +0700

    Update and rename untitled.sql to sales_data.sql

---
+++ capstone/module5/sales_data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE sales_data (
+  rowid INT PRIMARY KEY, 
+  product_id INT, 
+  customer_id INT, 
+  price decimal DEFAULT 0.0 NOT NULL,
+  quantity INT,
+  timeestamp timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+  );
--- capstone/module5/untitled.sql
+++
@@ -1,2 +0,0 @@
-CREATE sales_data rowid, product_id, customer_id, price, quantity
-timeestamp. Load sales.csv into the table sales_data 
