33a2ec51128eeacf81c46ecae8e71b02bb3fdf4e
artificialrose <129745681+artificialrose@users.noreply.github.com>
2025-04-28 11:35:48 +0700

    Create process_web_log.py

---
+++ capstone/module5/process_web_log.py
@@ -0,0 +1,83 @@
+'''
+
+pip install apache-airflow
+
+
+mkdir /home/project/airflow/dags/capstone
+cd /home/project/airflow/dags/capstone
+curl -O https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0321EN-SkillsNetwork/ETL/accesslog.txt
+
+'''
+
+
+
+# import libraries 
+from datetime import timedelta
+# The DAG object; we'll need this to instantiate a DAG
+from airflow.models import DAG
+# Operators; you need this to write tasks!
+from airflow.operators.bash_operator import BashOperator
+# This makes scheduling easy
+from airflow.utils.dates import days_ago
+
+
+# define DAG args 
+default_args = {
+    'owner' : 'Susan Rose',
+    'start_date' : days_ago(0),
+    'email':'dummymail.hsu@gmail.com',
+    'email_on_failure' : True,
+    'email_on_retry' : True,
+    'retries' : 1,
+    'retry_delay' : timedelta(minutes=5),
+}
+
+# define DAG 
+dag = DAG(
+    'process_web_log',
+    schedule_interval = timedelta(days=1),
+    default_args = default_args,
+    description = 'capstone project Apache Airflow',
+)
+
+# define task extract_data
+
+extract_data = BashOperator ( 
+    task_id = 'extract_data',
+    bash_command = 'cut -d" " -f1 /home/project/airflow/dags/capstone/accesslog.txt > /home/project/airflow/dags/capstone/extracted_data.txt',
+    dag=dag,
+)
+
+# define task transform_data
+
+transform_data = BashOperator ( 
+    task_id = 'transform_data',
+    bash_command = 'cat /home/project/airflow/dags/capstone/extracted_data.txt | grep "198.46.149.143" > /home/project/airflow/dags/capstone/transformed_data.txt' ,
+    dag=dag,
+)
+
+
+# define task load_data
+
+load_data = BashOperator ( 
+    task_id = 'load_data',
+    bash_command = 'tar -cvf /home/project/airflow/dags/capstone/weblog.tar /home/project/airflow/dags/capstone/transformed_data.txt' ,
+    dag=dag,
+)
+
+# task pipeline
+extract_data >> transform_data >> load_data
+
+'''
+export AIRFLOW_HOME=/home/project/airflow
+echo $AIRFLOW_HOME
+
+sudo cp process_web_log.py $AIRFLOW_HOME/dags
+
+'''
+
+
+
+# airflow dags list-import-errors
+
+# airflow dags unpause process_web_log
